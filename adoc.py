dd529fa08340273c675cc027cf663f5ae450f2d6
S. Andronov <andronovss@icloud.com>
2023-06-12 10:24:02 +0300

    added the new file:   adoc.py

---
+++ adoc.py
@@ -0,0 +1,45 @@
+import os
+
+def create_directory(path):
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def split_file(filename):
+    with open(filename, 'r', encoding='utf-8') as file:
+        content = file.readlines()
+
+    current_header = ''
+    current_content = ''
+    header_found = False
+
+    for line in content:
+        if line.startswith('='):
+            if header_found:
+                # Save the previous content to a file
+                save_file(current_header, current_content)
+            
+            current_header = line.strip('= \n')
+            current_content = ''
+            header_found = True
+        else:
+            current_content += line
+    
+    # Save the last section to a file
+    if header_found:
+        save_file(current_header, current_content)
+
+def save_file(header, content):
+    # Remove invalid characters from the header
+    header = ''.join(c for c in header if c.isalnum() or c.isspace())
+    
+    # Create a directory for the file
+    directory = os.path.join('output', header)
+    create_directory(directory)
+    
+    # Save the content to a file with ".adoc" extension
+    filename = os.path.join(directory, header + '.adoc')
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(content)
+
+# Replace 'input.adoc' with the path to your source file
+split_file('input.adoc')
